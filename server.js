cf8ff051863929b9898926b6096ee4e3401eaff8
Unknownaod <unknown@dona.pw>
2026-09-08 06:30:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,41 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (
+    origin ===
+    "https://minigames.memplace.xyz"
+  ) {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      origin
+    );
+
+    res.setHeader(
+      "Access-Control-Allow-Credentials",
+      "true"
+    );
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  next();
+});
+
 /*
 ==========================================
 LOAD API HANDLER
